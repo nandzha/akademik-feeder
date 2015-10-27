e88a710edd128fdac92b187b2e842d23e957960f
Baihaqy Aviq <aviq.baihaqy@gmail.com>
2015-10-27 03:50:18 +0000

    Delete vendor

--- vendor/dhtmlx/connector-php/codebase/Dhtmlx/Connector/DataStorage/ArrayQueryWrapper.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Dhtmlx\Connector\DataStorage;
-
-class ArrayQueryWrapper {
-    public function __construct($data){
-        $this->data = $data;
-        $this->index = 0;
-    }
-}
